199cda6bdac583eb27ddf35e2caeddd33daa075f
trần hạnh <trancu962004@gmail.com>
2026-06-26 10:35:31 +0700

    Update website

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'tim đây em bấm vào đi',
-    title: 'Phải chăng anh đã say ngay cái mặt xinh xinh này mất tiu luôn rồi',
+    btnIntro: 'tim đây đi vào đi em',
+    title: 'Phải chăng anh đã say cái mặt xinh xinh này mất tiu luôn rồi',
     desc: 'kỳ này phải hốt về nui thôi ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'tất nhiên là ưng rồi <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hãy ở lại TK đi Tối nay anh qua đón đi chơi ',
+    messDesc: 'Ở lại TK đi Tối nay anh qua đón đi chơi.... chiuk hongg ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/tran.hanh.21998' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
